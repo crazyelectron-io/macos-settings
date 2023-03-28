3a05bef4e1cb7c5566956c6c20cbf3bdacc45917
Ron Moerman <ron@crazyelectron.io>
2023-03-28 13:54:56 +0200

    Update 00-install.sh

--- 00-install.sh
+++ 00-install.sh
@@ -17,13 +17,13 @@ echo "...Install Rosetta2"
 
 echo "...Install usefull tools."
 brew install neovim ansible keepassxc terraform nmap links geoip bash-completion watch zsh
-echo "[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"" >>~/.bash_profile
+echo [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh" >>~/.bash_profile
 
 echo "...Create symlink to SSH directory. *** Make sure the Shared folder is synced ***"
 ln -s -v -f ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Shared/ssh ~/.ssh
 
 echo "...Install Oh My Zsh."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)”
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "...Install Oh My ZSH plugins."
 git clone https://github.com/zsh-users/zsh-docker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-docker
@@ -34,4 +34,4 @@ git clone https://github.com/powerline/fonts.git && cd fonts && ./install.sh
 echo "...Install powerlevel10k."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-echo "...Add `` to `~/.zshrc`, run `source ~/.zshrc` and configure powerlevel10k with `p10k configure`."
+echo "...Add `ZSH_THEME="powerlevel10k/powerlevel10k` to `~/.zshrc`, run `source ~/.zshrc` and configure powerlevel10k with `p10k configure`."
