ed20f8cb2204b330a74dcdc6b8b28f952bc5adaa
Ron Moerman <ron@crazyelectron.io>
2023-03-28 13:53:08 +0200

    Update 00-install.sh

--- 00-install.sh
+++ 00-install.sh
@@ -17,6 +17,21 @@ echo "...Install Rosetta2"
 
 echo "...Install usefull tools."
 brew install neovim ansible keepassxc terraform nmap links geoip bash-completion watch zsh
+echo "[[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh"" >>~/.bash_profile
+
+echo "...Create symlink to SSH directory. *** Make sure the Shared folder is synced ***"
+ln -s -v -f ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Shared/ssh ~/.ssh
 
 echo "...Install Oh My Zsh."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)”
+
+echo "...Install Oh My ZSH plugins."
+git clone https://github.com/zsh-users/zsh-docker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-docker
+
+echo "...Install the Powerline fonts."
+git clone https://github.com/powerline/fonts.git && cd fonts && ./install.sh
+
+echo "...Install powerlevel10k."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+echo "...Add `` to `~/.zshrc`, run `source ~/.zshrc` and configure powerlevel10k with `p10k configure`."
