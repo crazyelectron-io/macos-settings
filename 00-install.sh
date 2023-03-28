03ae04047853780db3ba4a6eede0bafe3d8d8741
Ron Moerman <ron@crazyelectron.io>
2023-03-28 14:28:05 +0200

    Update 00-install.sh

--- 00-install.sh
+++ 00-install.sh
@@ -12,36 +12,4 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 echo "...Install iTerm2."
 brew install iterm2
 
-echo "...Install Rosetta2"
-/usr/sbin/softwareupdate --install-rosetta --agree-to-license
-
-echo "...Make sure the Shared folder is downloaded."
-find ~/Library/Mobile Documents/com~apple~CloudDocs/Shared -name '.*icloud' | sed 's|\.\([^/]*\)\.icloud$|\1|g' | while read fn; do brctl download "$fn"; done
-
-echo "...Install usefull tools."
-brew install neovim ansible keepassxc terraform nmap links geoip bash-completion watch zsh gnu-sed grep
-echo [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]] && . "/opt/homebrew/etc/profile.d/bash_completion.sh" >>~/.bash_profile
-
-echo "...Create symlink to SSH directory."
-ln -s -v -f ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Shared/ssh ~/.ssh
-
-echo "...Install Oh My Zsh."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-echo "...Install Oh My ZSH plugins."
-git clone https://github.com/zsh-users/zsh-docker.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-docker
-
-echo "...Install the Powerline fonts."
-git clone https://github.com/powerline/fonts.git && cd fonts && ./install.sh
-
-echo "...Install powerlevel10k."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
-echo "...Add powerlevel10k as theme to ZSH."
-gsed -i 's+ZSH_THEME=\"robbyrussel\"+ZSH_THEME=\"powerlevel10k/powerlevel10k\"+g' ~/.zshrc
-source ~/.zshrc
-
-echo "...Configure powerlevel10k with `p10k configure`."
-p10k configure
-
-echo ;echo "...Exit and restart iterm2 (pin it to the Dock)."
+echo ;echo "...Exit Terminal, pin iTerm2 to Dock and start iterm2."
